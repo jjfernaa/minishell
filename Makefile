ea17aeeba17b9ceaf5a5a25e5b98579909ac74f6
dponce-g <dponce-g@student.42malaga.com>
2025-07-14 15:44:57 +0200

    Implementacion funcion parse_tokens. Creacion regla test en el makefile para automatizar testeos con un main_tester, pendiente de arreglar errores

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 NAME = minishell
+TEST = tester
 
 CC = cc
 CFLAGS = -Wall -Werror -Wextra
@@ -6,9 +7,13 @@ LIBFT_DIR = libft
 LIBFT = $(LIBFT_DIR)/libft.a
 INCLUDES = -I includes -I libft
 LEXER_DIR = srcs/lexer
-SRCS = srcs/main.c \
+PARSER_DIR = srcs/parser
+SRC_MAIN = srcs/main.c
+SRC_TESTER = tester/main_test.c
+SRCS =  \
 	$(LEXER_DIR)/lexer.c \
 	$(LEXER_DIR)/lexer_utils.c \
+	$(PARSER_DIR)/parser.c \
 	srcs/execution/command.c \
 	srcs/builtins/pwd.c \
 	srcs/builtins/echo.c \
@@ -19,7 +24,8 @@ SRCS = srcs/main.c \
 	srcs/builtins/unset.c \
 	srcs/parsing/split.c
 	
-OBJS = $(SRCS:.c=.o)
+OBJS = $(SRC_MAIN:.c=.o) $(SRCS:.c=.o)
+OBJS_TESTER = $(SRC_TESTER:.c=.o) $(SRCS:.c=.o)
 
 all: $(NAME)
 
@@ -29,17 +35,25 @@ $(LIBFT):
 $(NAME): $(OBJS) $(LIBFT)
 	$(CC) $(CFLAGS) $(OBJS) $(LIBFT) -lreadline -o $(NAME)
 
+$(TEST): $(OBJS_TESTER) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJS_TESTER) $(LIBFT) -lreadline -o $(TEST)
+
 %.o: %.c
 	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
 
+test: $(TEST)
+	./$(TEST)
+
 clean:
 	rm -f $(OBJS)
+	rm -f $(OBJS_TESTER)
 	make clean -C $(LIBFT_DIR)
 
 fclean: clean
 	rm -f $(NAME)
+	rm -f $(TEST)
 	make fclean -C $(LIBFT_DIR)
 
 re: fclean all
 
-.PHONY: all clean fclean re
+.PHONY: all clean fclean re test
--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -31,7 +31,71 @@ static void	cmd_add_back(t_cmd **cmd_list, t_cmd *new)
 	tmp->next = new;
 }
 
+static char	**add_to_argv(char **argv, char *word)
+{
+	char	**new;
+	int		i;
+	int		len;
+
+	len = 0;
+	while (argv && argv[len])
+		len++;
+	new = malloc(sizeof(char *) * (len + 2));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		new[i] = argv[i];
+		i++;
+	}
+	new[i++] = ft_strdup(word);
+	new[i] = NULL;
+	free(argv);
+	return (new);
+}
+
+static void	handle_redirection(t_token *tokens, t_cmd *current)
+{
+	if (tokens->type == T_REDIR_IN || tokens->type == T_HEREDOC)
+	{
+		current->infile = ft_strdup(tokens->next->value);
+		if (tokens->type == T_HEREDOC)
+			current->heredoc = 1;
+	}
+	else if (tokens->type == T_REDIR_OUT || tokens->type == T_APPEND)
+	{
+		current->outfile = ft_strdup(tokens->next->value);
+		if (tokens->type == T_APPEND)
+			current->append = 1;
+	}
+}
+
 t_cmd	*parse_tokens(t_token *tokens)
 {
+	t_cmd	*cmds;
+	t_cmd	*current;
 
+	cmds = NULL;
+	current = new_cmd();
+	while (tokens)
+	{
+		if (tokens->type == T_WORD)
+			current->argv = add_to_argv(current->argv, tokens->value);
+		else if (tokens->type == T_PIPE)
+		{
+			cmd_add_back(&cmds, current);
+			current = new_cmd();
+		}
+		else if (tokens->type == T_REDIR_IN || tokens->type == T_HEREDOC 
+				|| tokens->type == T_REDIR_OUT || tokens->type == T_APPEND)
+		{
+			if (!tokens->next || tokens->next->type != T_WORD)
+				return (NULL);
+			handle_redirection(tokens, current);
+		}
+		tokens = tokens->next;
+	}
+	cmd_add_back(&cmds, current);
+	return (cmds);
 }
