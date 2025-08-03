b8bdeb828fa6297ff47ecb712e233adc1b3c04c0
dponce-g <dponce-g@student.42malaga.com>
2025-08-03 19:20:05 +0200

    Revision tras pull request. Separacion de funciones en env_to_array, cumpliendo norminette 25 lineas, para hacerla mas robusta en el caso de fallo y retorno Null de ft_strjoin, donde liberamos todo y devolvemos Null

--- srcs/env/env_to_array.c
+++ srcs/env/env_to_array.c
@@ -1,4 +1,19 @@
 #include "env.h"
+// Eliminar y quedarnos solo con un free_array. Por ejemplo, crear un utils.h
+static void	free_array_envp(char **array)
+{
+	int	i;
+
+	if (!array)
+		return ;
+	i = 0;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
 
 static int	env_size(t_env *env)
 {
@@ -14,25 +29,46 @@ static int	env_size(t_env *env)
 	return (count);
 }
 
+static char	*create_envp_line(t_env *env, char **envp)
+{
+	char	*joined_key_equal;
+	char	*envp_line;
+
+	joined_key_equal = ft_strjoin(env->key, "=");
+	if (!joined_key_equal)
+	{
+		free_array_envp(envp);
+		return (NULL);
+	}
+	envp_line = ft_strjoin(joined_key_equal, env->value);
+	if (!envp_line)
+	{
+		free(joined_key_equal);
+		free_array_envp(envp);
+		return (NULL);
+	}
+	free(joined_key_equal);
+	return (envp_line);
+}
+
 char	**env_to_array(t_env *env)
 {
 	char	**envp;
 	int		size;
 	int		i;
-	char	*joined_key_equal;
 
 	size = env_size(env);
 	envp = malloc(sizeof(char *) * (size + 1));
 	if (!envp)
 		return (NULL);
 	i = 0;
-	while(env)
+	while (env)
 	{
 		if (env->key && env->value)
 		{
-			joined_key_equal = ft_strjoin(env->key, "=");
-			envp[i] = ft_strjoin(joined_key_equal, env->value);
-			free(joined_key_equal);
+			envp[i] = create_envp_line(env, envp);
+			if (!envp[i])
+				return NULL;
 			i++;
 		}
 		env = env->next;
