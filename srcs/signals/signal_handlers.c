c6e92030f59b97733eceac71b614b7c320785050
Juan Fernández <juanjose.fernanfez88@gmail.com>
2025-08-14 05:31:14 +0200

    señales Ctrl+C funcionando

--- srcs/signals/signal_handlers.c
+++ srcs/signals/signal_handlers.c
@@ -1,14 +1,13 @@
 #include "../../includes/minishell.h"
 
-extern volatile sig_atomic_t	g_signal_received;
+//extern volatile sig_atomic_t	g_signal_received;
 
 void	handle_sigint(int sig)
 {
 	(void)sig;
 	g_signal_received = SIGINT;
-	write(STDERR_FILENO, "\n", 1);
+	write(STDERR_FILENO, "\nminishell$ ", 12);
 	rl_on_new_line();
 	//rl_replace_line("", 0);
 	rl_redisplay();
 }
-
