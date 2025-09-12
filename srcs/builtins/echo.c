d90e33352fef2ac6095258d68087243e2858600a
Juan Jose Fernandez <juan-jof@c3r5s1.42malaga.com>
2025-09-12 23:05:27 +0200

    echo_modificacion

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,11 +6,27 @@
 /*   By: juan-jof <juan-jof@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 01:22:48 by juan-jof          #+#    #+#             */
-/*   Updated: 2025/08/15 05:06:55 by juan-jof         ###   ########.fr       */
+/*   Updated: 2025/09/12 22:37:36 by juan-jof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+static int	is_valid_n_flag(char *arg)
+{
+	int	j;
+
+	if (!arg || arg[0] != '-' || arg[1] != 'n')
+		return (0);
+	j = 2;
+	while (arg[j])
+	{
+		if (arg[j] != 'n')
+			return (0);
+		j++;
+	}
+	return (1);
+}
 
 int	builtin_echo(char **args, t_shell *shell)
 {
@@ -20,10 +36,10 @@ int	builtin_echo(char **args, t_shell *shell)
 	(void)shell;
 	i = 1;
 	newline = 1;
-	if (args[1] && ft_strcmp(args[1], "-n") == 0)
+	while (args[i] && is_valid_n_flag(args[i]))
 	{
 		newline = 0;
-		i = 2;
+		i++;
 	}
 	while (args[i])
 	{
