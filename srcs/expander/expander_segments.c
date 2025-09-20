56b3c7bc7ea2552b5a97fd23f3ed625120ec7a53
Juan Jose Fernandez <juan-jof@c3r8s5.42malaga.com>
2025-09-20 17:53:46 +0200

    norma

--- srcs/expander/expander_segments.c
+++ srcs/expander/expander_segments.c
@@ -2,62 +2,62 @@
 
 static char	*expand_single_segment(t_token_segment *segment, t_env *env, int exit_status)
 {
-    if (segment->quote_type == SINGLE_QUOTE)
-        return (ft_strdup(segment->content));
-    else if (segment->quote_type == DOUBLE_QUOTE || segment->quote_type == NO_QUOTE)
-        return (expand_string(segment->content, env, exit_status));
-    else
-        return (ft_strdup(segment->content));
+	if (segment->quote_type == SINGLE_QUOTE)
+		return (ft_strdup(segment->content));
+	else if (segment->quote_type == DOUBLE_QUOTE || segment->quote_type == NO_QUOTE)
+		return (expand_string(segment->content, env, exit_status));
+	else
+		return (ft_strdup(segment->content));
 }
 
 static char	*join_expanded_segments(char *result, char *expanded_segment)
 {
-    char	*temp;
+	char	*temp;
 
-    temp = ft_strjoin(result, expanded_segment);
-    free(result);
-    free(expanded_segment);
-    return (temp);
+	temp = ft_strjoin(result, expanded_segment);
+	free(result);
+	free(expanded_segment);
+	return (temp);
 }
 
 char	*expand_token_with_segments(t_token *token, t_env *env, int exit_status)
 {
-    char			*result;
-    char			*expanded_segment;
-    t_token_segment	*current;
+	char			*result;
+	char			*expanded_segment;
+	t_token_segment	*current;
 
-    if (!token->segments)
-        return (expand_string(token->value, env, exit_status));
-    result = ft_strdup("");
-    current = token->segments;
-    while (current)
-    {
-        expanded_segment = expand_single_segment(current, env, exit_status);
-        result = join_expanded_segments(result, expanded_segment);
-        current = current->next;
-    }
-    return (result);
+	if (!token->segments)
+		return (expand_string(token->value, env, exit_status));
+	result = ft_strdup("");
+	current = token->segments;
+	while (current)
+	{
+		expanded_segment = expand_single_segment(current, env, exit_status);
+		result = join_expanded_segments(result, expanded_segment);
+		current = current->next;
+	}
+	return (result);
 }
 
 void	expand_var(t_shell *shell)
 {
-    t_token	*current;
-    char	*old_value;
-    char	*new_value;
+	t_token	*current;
+	char	*old_value;
+	char	*new_value;
 
-    current = shell->tokens;
-    while (current)
-    {
-        if (current->type == T_WORD)
-        {
-            old_value = current->value;
-            new_value = expand_token_with_segments(current, shell->env, shell->exit_status);
-            if (new_value)
-            {
-                current->value = new_value;
-                free(old_value);
-            }
-        }
-        current = current->next;
-    }
+	current = shell->tokens;
+	while (current)
+	{
+		if (current->type == T_WORD)
+		{
+			old_value = current->value;
+			new_value = expand_token_with_segments(current, shell->env, shell->exit_status);
+			if (new_value)
+			{
+				current->value = new_value;
+				free(old_value);
+			}
+		}
+		current = current->next;
+	}
 }
--- srcs/lexer/lexer_concatenation.c
+++ srcs/lexer/lexer_concatenation.c
@@ -3,57 +3,57 @@
 
 static void	update_token_quote_type(t_token *token, t_quote_type part_quote_type, int is_first)
 {
-    if (is_first)
-        token->quote_type = part_quote_type;
+	if (is_first)
+		token->quote_type = part_quote_type;
 }
 
 static char	*concatenate_part(char *result, const char *part)
 {
-    char	*temp;
+	char	*temp;
 
-    temp = ft_strjoin(result, part);
-    free(result);
-    return (temp);
+	temp = ft_strjoin(result, part);
+	free(result);
+	return (temp);
 }
 
 static int	process_segment(t_token *token, const char *input, int *i, char **result, int *first_token)
 {
-    char			*part;
-    t_quote_type	part_quote_type;
+	char			*part;
+	t_quote_type	part_quote_type;
 
-    part = read_segment(input, i, &part_quote_type);
-    if (!part)
-        return (0);
-    add_segment_to_token(token, part, part_quote_type);
-    update_token_quote_type(token, part_quote_type, *first_token);
-    *first_token = 0;
-    *result = concatenate_part(*result, part);
-    free(part);
-    return (*result != NULL);
+	part = read_segment(input, i, &part_quote_type);
+	if (!part)
+		return (0);
+	add_segment_to_token(token, part, part_quote_type);
+	update_token_quote_type(token, part_quote_type, *first_token);
+	*first_token = 0;
+	*result = concatenate_part(*result, part);
+	free(part);
+	return (*result != NULL);
 }
 
 void	handle_word_with_concatenation(t_token **list, const char *input, int *i)
 {
-    char	*result;
-    t_token	*token;
-    int		first_token;
+	char	*result;
+	t_token	*token;
+	int		first_token;
 
-    result = ft_strdup("");
-    token = add_token(list, T_WORD, "");
-    first_token = 1;
-    while (input[*i] && !ft_isspace(input[*i]) && !is_symbol(input[*i]))
-    {
-        if (!process_segment(token, input, i, &result, &first_token))
-        {
-            free(result);
-            return ;
-        }
-    }
-    if (token)
-    {
-        free(token->value);
-        token->value = result;
-    }
-    else
-        free(result);
+	result = ft_strdup("");
+	token = add_token(list, T_WORD, "");
+	first_token = 1;
+	while (input[*i] && !ft_isspace(input[*i]) && !is_symbol(input[*i]))
+	{
+		if (!process_segment(token, input, i, &result, &first_token))
+		{
+			free(result);
+			return ;
+		}
+	}
+	if (token)
+	{
+		free(token->value);
+		token->value = result;
+	}
+	else
+		free(result);
 }
